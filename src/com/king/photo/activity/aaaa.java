14f0fbd5325e1869d3173f9f7e273e158f5ed0c5
zj <z177175900@sina.com>
2016-02-17 16:52:47 +0800

    223232

--- src/com/king/photo/activity/aaaa.java
+++ src/com/king/photo/activity/aaaa.java
@@ -1,5 +1,15 @@
 package com.king.photo.activity;
 
-public class aaaa {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
 
+public class aaaa extends Activity{
+	private ListView lv;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	
+}
 }
